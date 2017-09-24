7f2a0c787a5bc11249506c068257d776c1edf477
Roman <sektor_wins@mail.ru>
2017-09-24 21:03:34 +0500

    Добавлена библиотека matplotlib; построен график зависимости веса от роста

--- script.py
+++ script.py
@@ -1,6 +1,14 @@
+# -*- coding: utf8 -*- 
 import pandas
-
+import matplotlib.pyplot as plt
 
 data_for_computing = pandas.read_csv("data.csv", sep=",", index_col="Index")
 
-print (data_for_computing)
+#print (data_for_computing["Weight"].tolist())
+plt.plot(data_for_computing["Height"].values, data_for_computing["Weight"].values,'go')
+plt.xlabel('Рост (дюймы)') #Метка по оси x в формате TeX
+plt.ylabel('Вес (футы)') #Метка по оси y в формате TeX
+plt.title('Зависимость веса от роста') #Заголовок в формате TeX
+plt.grid(True) #Сетка
+plt.show()
+#print (data_for_computing)
